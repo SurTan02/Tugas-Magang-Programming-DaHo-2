2b1e124e24be76a7397beb73103a769d00f75f69
SurTan02 <73408389+SurTan02@users.noreply.github.com>
2021-07-06 21:46:12 +0700

    Update subscriber.cpp

--- src/1_Publisher-Subscriber/src/subscriber.cpp
+++ src/1_Publisher-Subscriber/src/subscriber.cpp
@@ -1,1 +1,45 @@
 // TODO: implementasi subscriber.cpp
+
+
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include <iostream>
+
+class MsgSubscriber{
+	private:
+		ros::NodeHandle n;
+		ros::Subscriber sub;
+		int counter;
+		
+	public:
+		MshSubscriber(){
+			sub = n.subscribe<std_msgs::String>("chatter", 1000, &MsgSubscriber::chatterCallback,this)
+		}
+		
+		void chatterCallback(const std_msgs::String::ConstPtr& msg){
+			msg->data.c_str();
+		}
+			
+		void print(){
+			cout<<"counter: "<<counter <<endl;
+		}
+};
+
+int main(int argc, char **argv) {
+
+    ros::init(argc, argv, "subscriber");
+
+    MsgSubscriber ns;
+
+    ROS_INFO("subscriber running...");
+
+    ros::Rate rate(60);
+
+    while(ros::ok()) {
+        ns.print();
+        ros::spinOnce();
+        rate.sleep();
+    }
+
+    return 0;
+}
