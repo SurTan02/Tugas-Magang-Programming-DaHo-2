53f3a8e1daae578c6da6908069769a3569ea813c
SurTan02 <73408389+SurTan02@users.noreply.github.com>
2021-07-09 21:56:03 +0700

    Update publisher.cpp

--- src/1_Publisher-Subscriber/src/publisher.cpp
+++ src/1_Publisher-Subscriber/src/publisher.cpp
@@ -1,46 +1,38 @@
-// TODO: implementasi publisher.cpp
+//Suryanto
+//16520195
 
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 
 #include <sstream>
 
-class MsgPublisher {
-	private :
-		ros::NodeHandle n;
-		ros:: Publisher chatter_pub;
-	
-	public:
-		MsgPublisher(){
-			chatter_pub = n.advertise <std_msgs::String>("chatter",1000);
-		}
-		
-		void process(String word){
-			std_msgs::String msg;
-			msg.data = data;
-			chatter_pub.publish(msg);
-		}
-		
-};
+
+int main(int argc, char **argv)
+{
+  
+  ros::init(argc, argv, "publisher");
+
+  
+  ros::NodeHandle n;
+
+  ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
+
+  ros::Rate loop_rate(60);
+ 
+  while (ros::ok()){
+    std_msgs::String msg;
+
+    std::stringstream ss;
+    ss << "Hello, World! ";
+    msg.data = ss.str();
+	ROS_INFO("%s", msg.data.c_str());
+    chatter_pub.publish(msg);
+
+    ros::spinOnce();
+
+    loop_rate.sleep();
+  }
 
 
-
-int main(int argc, char **argv){
-	
-ros::init(argc, argv, "publisher");
-
-    MsgPublisher np;
-
-    ROS_INFO("publisher running...");
-
-    ros::Rate loop_rate(60);
-
-    while (ros::ok()) {
-
-        np.process("Hello, World!");
-
-        loop_rate.sleep();
-    }
-
-    return 0;
+  return 0;
 }
